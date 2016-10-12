cd78268bc7d84dd53cf67e63fe0ffd0935390955
Alex <pyxstar@hotmail.com>
2016-10-12 17:14:23 +1300

    Edited comments

--- src/test/TranStatsMain.java
+++ src/test/TranStatsMain.java
@@ -5,8 +5,8 @@ import algoSimba.TransactionStatsGenerator;
 public class TranStatsMain {
 
 	public static void main(String[] args) {
-		String inputFile = "C:\\Users\\Alex\\Desktop\\760\\A2\\market\\market.dat";
-		String fileName = "C:\\Users\\Alex\\Desktop\\760\\A2\\supports.csv";
+		String inputFile = "path to the input file";
+		String fileName = "path to the ouput file"; //if the file does not exist, a new file will be created
 		try{
 			TransactionStatsGenerator transDBStats = new TransactionStatsGenerator(); 
 			transDBStats.getStats(inputFile, fileName);
